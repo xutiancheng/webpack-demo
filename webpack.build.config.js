365515b1de3d4db2adfce98f3ec3f0e76b7d1502
twt313@gmail.com <twt313@gmail.com>
2017-04-20 17:31:47 +0800

    'add'

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -15,13 +15,13 @@ module.exports = {
             test: /\.scss$/,
             loader: ExtractTextPlugin.extract({
                   fallback: "style-loader",
-                  use: "css-loader!sass-loader"
+                  use: "css-loader!postcss-loader!sass-loader"
             })
         },{
             test: /\.css$/,
             loader:ExtractTextPlugin.extract({
               fallback: "style-loader",
-              use: "css-loader"
+              use: "css-loader!postcss-loader"
             })
         },{
             test: /\.(png|jpg|git|svg)$/i,
