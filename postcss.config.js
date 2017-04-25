3c619a4e3f1f3f05e79048e2c63fd8c4a2ebf97a
twt313@gmail.com <twt313@gmail.com>
2017-04-25 17:16:11 +0800

    'save'

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 var px2rem = require('postcss-px2rem');
 module.exports = {
   plugins: [
-    require('autoprefixer')({browsers:'ios >= 8'}),px2rem({remUnit:75})
+    //require('autoprefixer')({browsers:'ios >= 8'}),px2rem({remUnit:75})
   ]
 }
--- src/script/index.js
+++ src/script/index.js
@@ -2,18 +2,25 @@ require('../style/reset.scss');
 require('../style/swiper-3.4.2.min.css');
 require('../style/main.scss');
 require('../script/lib-flexible.min');
+import $ from 'jquery'
+
 // var attachFastClick = require('fastclick');  
 // attachFastClick(document.body);  
 
  var mySwiper = new Swiper ('.swiper-container', {
-    loop: true,
-    
+    loop: true,    
     // 如果需要分页器
     pagination: '.swiper-pagination',
-  }) 
+  }); 
 
+$(window).resize(function(){
+ mySwiper.destroy(false)
+ setTimeout(function(){
+ 	 mySwiper = new Swiper ('.swiper-container', {
+    loop: true,    
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+  }); 
+ },300);
 
-function function_name (argument) {
-	// body...
-	cosnole.log(1);
-}
+});
